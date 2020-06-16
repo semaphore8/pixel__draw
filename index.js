c9a2655c112856bc7e30235a94424d72b10c29bc
sgbliznyuk <sgbliznyuk@gmail.com>
2020-06-16 15:15:31 +0300

    added document ready waiting

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-<document className="getE">
-
-</document>
-
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById("app").innerHTML = `
+        <h1>pixel__draw</h1>
+    `
+})
