ca35c089982a9cec6c215955ef07f368f6071ab3
sgbliznyuk <sgbliznyuk@gmail.com>
2020-06-17 16:17:38 +0300

    added tool select and colorselect

--- index.js
+++ index.js
@@ -97,6 +97,55 @@ function pointerPosition(pos, domNode) {
             y: Math.floor((pos.clientY - rect.top) / scale)};
 }
 
+class PixelEditor {
+    constructor(state, config) {
+        let {tools, controls, dispatch} = config;
+        this.state = state;
+
+        this.canvas = new PictureCanvas(state.picture, pos => {
+            let tool = tools[this.state.tool];
+            let onMove = tool(pos, this.state, dispatch);
+            if (onMove) return pos => onMove(pos, this.state);
+        });
+        this.controls = controls.map(
+            Control => new Control(state, config));
+            this.dom = elt("div", {}, this.canvas.dom, elt("br"),
+                            ...this.controls.reduce(
+                                (a, c) => a.concat(" ", c.dom), []
+                            ));
+    }
+    syncState(state) {
+        this.state = state;
+        this.canvas.syncState(state.picture);
+        for (let ctrl of this.controls) ctrl.syncState(state);
+    }
+}
+
+class ToolSelect {
+    constructor(state, {tools, dispatch}) {
+        this.select = elt("select", {
+            onchange: () => dispatch({tool: this.select.value})
+        }, ... Object.keys(tools).map(name => elt("option", {
+            selected: name == state.tool
+        }, name)))
+        this.dom = elt("label", null, "Tool: ", this.select);
+    }
+    syncState(state) { this.select.value = state.tool; }
+}
+
+class ColorSelect {
+    constructor(state, {dispatch}) {
+        this.input = elt("input", {
+            type: "color",
+            value: state.color,
+            onchange: () => dispatch({color: this.input.value})
+        });
+        this.dom = elt("label", null, "Color: ", this.input);
+    }
+    syncState(state) { this.input.value = state.color; }
+}
+
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById("app").innerHTML = `
